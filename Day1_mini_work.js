f5b6e59b2510ffaa2d9cb4dc298b94c649270025
Rishith Sai Chowdary Chundru <rishithrishith1@gmail.com>
2025-06-16 14:05:50 +0530

    Mini Web Page using Javascript

---
+++ Day1_mini_work.js
@@ -0,0 +1,5 @@
+const button=document.getElementById("click")
+const message=document.getElementById("text")
+button.addEventListener("click",function(){
+    message.textContent="I am Pursuing my Btech from LPU"
+});
