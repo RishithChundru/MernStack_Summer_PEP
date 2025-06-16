18e32dc5da56695ad3bf49a5241f3d61aa868dc4
Rishith Sai Chowdary Chundru <rishithrishith1@gmail.com>
2025-06-16 13:31:06 +0530

    Javascript Basics

---
+++ String_Functions.js
@@ -0,0 +1,33 @@
+var str="Rishith"
+var s="sai"
+// length function
+console.log(str.length)
+
+
+// charAt function
+console.log(str.charAt(3))
+
+// charCodeAt function
+console.log(str.charCodeAt('R'))
+
+// substring function
+console.log(str.substring(0,3))
+
+// Uppercase function
+console.log(str.toUpperCase())
+
+// Lowercase function
+console.log(str.toLowerCase())
+
+// concat function
+console.log(str.concat(" ",s))
+
+// trim function
+console.log(str.trim())
+
+// replace function
+console.log(str.replace('R','H'))
+
+// split function
+console.log(str.split())
+
---
+++ practice.js
@@ -0,0 +1,34 @@
+/*
+var a=10
+console.log
+*/
+
+// print name in console
+
+/*
+var name
+let name_1='Sai'
+const name_2='Rishith'
+console.log(name)
+console.log(name_1)
+console.log(name_2)
+*/
+
+// Add two numbers
+
+/*
+var a=10
+var b=20
+console.log("Addition:",a+b)
+*/
+
+// check if number is odd or even
+
+let input=prompt("Enter number: ");
+input = Number(input);
+if(input%2==0){
+    console.log("it is even");
+}
+else{
+    console.log("it is odd")
+}
